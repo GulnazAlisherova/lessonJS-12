257c7c6f5b7dcc7e5ac04a92ccc669fcf6648c6e
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2022-03-04 14:53:26 +0600

    added setitem

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-// const example1Span = document.querySelector('#example-1 button');
-// example1Span.textContent =localStorage.getItem('example-1-number') ?? 0;
-// document.querySelector('#example-1 button').addEventListener('click', function(){
-//   example1Span.textContent++;
-//   localStorage.setItem('example-1-number', example1Span.textContent);
+// // const example1Span = document.querySelector('#example-1 button');
+// // example1Span.textContent =localStorage.getItem('example-1-number') ?? 0;
+// // document.querySelector('#example-1 button').addEventListener('click', function(){
+// //   example1Span.textContent++;
+// //   localStorage.setItem('example-1-number', example1Span.textContent);
+// // });
+
+// document.querySelector('input');
+
+
+// const example3Input = document.querySelector('#example-3 input');
+// example3Input.value = localStorage.getItem('example-3-input');
+// example3Input.addEventListener('input', function(){
+//   localStorage.setItem('example-3-input', example3Input.value)
 // });
-
-document.querySelector('input');
-
-
-const example3Input = document.querySelector('#example-3 input');
-example3Input.value = localStorage.getItem('example-3-input');
-example3Input.addEventListener('input', function(){
-  localStorage.setItem('example-3-input', example3Input.value)
-});
