5313ed412f1d3c6e44967e110626b4933f3440d9
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2022-03-04 15:00:19 +0600

    added checked

--- script.js
+++ script.js
@@ -19,5 +19,5 @@ darkTheme.checked =+localStorage.getItem('dark-theme');
 document.body.classList.toggle('dark-theme', darkTheme.checked);
 //react
 darkTheme.addEventListener('changee', function(){
-  localStorage.setItem
+  localStorage.setItem('dark-theme', +darkTheme.checked)
 });
