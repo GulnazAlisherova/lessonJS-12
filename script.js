1801469eb62b565d0c13238e621ff669cd35d4bb
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2022-03-04 14:17:52 +0600

    added textContent

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const buton = document.querySelector('#example-1 button');
+const button = document.querySelector('#example-1 button');
 const span = document.querySelector('#example-1 span');
 
-button.addEventListner('click', function())
+button.addEventListner('click', function(){
+  span.textContent
+});
