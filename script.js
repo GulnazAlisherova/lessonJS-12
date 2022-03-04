8817935ae3c02eb4a5b9d7c3cbc29a24efe1c528
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2022-03-04 14:18:15 +0600

    added double ++

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const button = document.querySelector('#example-1 button');
 const span = document.querySelector('#example-1 span');
 
 button.addEventListner('click', function(){
-  span.textContent
+  span.textContent++;
 });
