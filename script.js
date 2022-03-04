78a7d952c2afe76f03e438a885d8ae0668c9bb52
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2022-03-04 14:40:56 +0600

    added div

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-const example1Span = document.querySelector('#example-1 button');
-example1Span.textContent = localStorage.getItem('example-1-number') ?? 0;
-document.querySelector('#example-1 button').addEventListener('click', function(){
-  example1Span.textContent++;
-  localStorage.setItem('example-1-number', example1Span.textContent);
-});
+// const example1Span = document.querySelector('#example-1 button');
+// example1Span.textContent =localStorage.getItem('example-1-number') ?? 0;
+// document.querySelector('#example-1 button').addEventListener('click', function(){
+//   example1Span.textContent++;
+//   localStorage.setItem('example-1-number', example1Span.textContent);
+// });
+
+document.querySelector('input');
+
+
