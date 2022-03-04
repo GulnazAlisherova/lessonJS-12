89cb9d4f70dad255b66db87806769fe902653254
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2022-03-04 14:26:42 +0600

    added addEventListener

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const example1Span = document.querySelector('#example-1 button');
 example1Span.textContent = localStorage.getItem('example-1-number') ?? 0;
-document.querySelector
+document.querySelector('#example-1 button').addEventListener
