d0ac811a39fbabb9be8a3ed66bdc2646645fce0a
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2022-03-04 14:20:47 +0600

    added span.textContent

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const button = document.querySelector('#example-1 button');
 const span = document.querySelector('#example-1 span');
 
-button.addEventListner('click', function(){
+button.addEventListener('click', function(){
   span.textContent++;
-  localStorage.setItem
+  localStorage.setItem('example-1-number', span.textContent)
 });
